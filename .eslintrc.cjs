f54a6be7282432f87380eff048407e0d4368299f
Lailnedx <113921786+Lailnedx@users.noreply.github.com>
2024-09-30 14:37:57 +0000

    Add SVG + Examples

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,26 +17,3 @@ module.exports = {
     '**/*.html'
   ]
 }
-{
-  "git.autofetch": true,
-    "[javascript]": {
-    "editor.defaultFormatter": "dbaeumer.vscode-eslint"
-  },
-  "[vue]": {
-    "editor.defaultFormatter": "dbaeumer.vscode-eslint"
-  },
-  "[html]": {
-    "editor.defaultFormatter": "vscode.html-language-features"
-  },
-  "editor.codeActionsOnSave": {
-    "source.formatDocument": "explicit",
-      "source.fixAll.eslint": "explicit"
-  },
-  "eslint.validate": [
-    "vue",
-    "html",
-    "javascript"
-  ],
-    "eslint.format.enable": true
-}
-
